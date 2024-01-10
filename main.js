1d53ef1abf7ef37cd341a399cb20914244ee7bf6
Dario Becchio <dabek1986@gmail.com>
2024-01-10 16:12:37 +0100

    create filter

--- main.js
+++ main.js
@@ -116,15 +116,49 @@ const elements= [
 		color: 'blue'
 	}
 ];
-const containerEl = document.getElementById('icon')
+const containerEl = document.getElementById('icon');
+
 elements.forEach((element) => {
     console.log(element);
     console.log(element.name); 
-    containerEl.innerHTML += `<div class="card col-1 d-flex flex-column"> 
+    containerEl.innerHTML += 
+	`
+	<div class="card col-3 p-1 my-1 d-flex align-items-center align-middle text-center dimension"> 
     <h3>${element.name} </h3> 
     <i class = "${element.family} ${element.prefix}solid ${element.prefix}${element.name} ${element.color}"></i> 
-    </div>`
+    </div>
+	
+	`
 })
+
+//Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
 document.getElementById('selected').addEventListener('change', function() {
-	console.log('You selected: ', this.value);
-  });
+	//console.log('You selected: ', );
+	let selectedOption = this.value;
+	console.log(selectedOption);
+	let selectedElement = elements
+		
+	
+	if (selectedOption !== 'all'){
+		
+		selectedElement = elements.filter((el) => el.type === selectedOption);	
+		console.log(selectedElement);					
+	}	
+
+	const htmlFilteredElements = selectedElement.map(el=>{
+		return `
+		<div class="card col-3 p-1 my-1 d-flex align-items-center align-self-center text-center dimension"> 
+		<h3>${el.name} </h3> 
+		<i class = "${el.family} ${el.prefix}solid ${el.prefix}${el.name} ${el.color}"></i> 
+		</div>		
+		`
+	})
+	containerEl.innerHTML = '';
+	htmlFilteredElements.forEach((el)=>{
+		containerEl.innerHTML += el;
+	})
+});
+
+
+//1 - modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
